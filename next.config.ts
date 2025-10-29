01ab779804b1ffeab4a65fcdc317eb498109ec5d
David <davidpradeslopez@gmail.com>
2025-10-29 22:22:58 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: process.env.NODE_ENV === 'production' ? '/Data-Insights-Portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Data-Insights-Portfolio/' : '',
   output: 'export',
   images: {
     unoptimized: true,
