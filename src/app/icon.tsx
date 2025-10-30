cfad3ab26314c4d5ad7d5b70b8ba2608ca7e615c
David <davidpradeslopez@gmail.com>
2025-10-30 16:23:49 +0100

    Create icon.tsx

---
+++ src/app/icon.tsx
@@ -0,0 +1,35 @@
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://i.imgur.com/VrbPcoo.png"
+          alt="Logo"
+          width={32}
+          height={32}
+        />
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
