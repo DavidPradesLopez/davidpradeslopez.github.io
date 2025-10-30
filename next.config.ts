99ae8e942b2f9dc4773b2f00fc590c88fc1e6cb8
David <davidpradeslopez@gmail.com>
2025-10-30 14:11:29 +0100

    Refactor next.config.ts to remove repo paths
    Removed repo-specific assetPrefix and basePath configurations.

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,17 @@
 import type {NextConfig} from 'next';
 
-const repo = 'Data-Insights-Portfolio';
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
-
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? assetPrefix : '',
-  basePath: process.env.NODE_ENV === 'production' ? basePath : '',
-  images: {
-    unoptimized: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
-
